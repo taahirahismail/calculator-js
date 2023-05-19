c842aad97e2e413862e561c5ea5c0ca227386334
taahirahismail <taahirah.i@gmail.com>
2023-05-19 20:57:21 +0200

    calculator kinda finished

--- script/index.js
+++ script/index.js
@@ -1,5 +1,22 @@
-let button = document.querySelectorAll('button');
+let screen = document.getElementById('display');
 
 function display(num) {
-    button.value += num
+    screen.value += num
+}
+
+function calc() {
+    screen.value = eval(screen.value)
+}
+
+function clr() {
+    screen.value = "";
+}
+
+function del() {
+    screen.value = screen.value.slice(0, -1)
+}
+
+let symbols = document.getElementsByClassName('.symbol')
+if (screen.value == symbols) {
+    screen.value = ''
 }
